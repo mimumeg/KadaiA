d44c2bfaa564d2e31fb8047b3776e010c5eb9148
mimumeg <meg.stella555@gmail.com>
2018-11-20 14:23:20 +0900

    A-11のTODOリストを記入した。 がんばる！

--- KadaiA.py
+++ KadaiA.py
@@ -1,6 +1,7 @@
 # A-1: 文字列のリスト
 # "Bob", "Tom", "Ken" という3つの文字列要素を持つusersというリストを定義してください
 from typing import List
+import random
 
 users = ["Bob", "Tom", "Ken"]
 
@@ -18,16 +19,20 @@ kazuma_info = ["Kazuma", "Takahashi", 35]
 # 次のmembersというリストから "Makoto" と "Kazuma" を取得して出力してください
 members = ["Kazuma", "Makoto", "Ohira"]
 
+print(f"-----A-4:-----")
 print(members[1])
 print(members[2])
+print()
 
 # A-5:  要素へのアクセスとフォーマット
 # 次のリストを利用して、"Name: Takahashi Kazuma, Age: 35"と出力してください
+print(f"-----A-5:-----")
 print(f'Name: {kazuma_info[1]} {kazuma_info[0]}, Age: {kazuma_info[2]}')
+print()
 
 # A-6: forによるループその1
 # for を使って odd_numbers の各要素を出力してください
-
+print(f"-----A-6:-----")
 odd_numbers = [1, 3, 5, 7, 9]
 for odd_number in odd_numbers:
     print(odd_number)
@@ -36,7 +41,7 @@ print()
 
 # A-7: forによるループその2
 # for を使って even_numbers のそれぞれの値を2倍した値を出力してください
-
+print(f"-----A-7:-----")
 even_numbers = [2, 4, 6, 8]
 for even_number in even_numbers:
     result = even_number * 2
@@ -51,6 +56,7 @@ print()
 # "Name: Tom, Age: 57"
 # "Name: Bob, Age: 77"
 
+print(f"-----A-8:-----")
 users_info = [["Kazuma", 35],
               ["Tom", 57],
               ["Bob", 77]]
@@ -61,16 +67,31 @@ for users_name in users_info:
     print(f"Name: {users_info[users_element][0]}, Age: {users_info[users_element][1]}")
     users_element += 1
 
+print()
 
 # A-9: 辞書
 # 下記のコードが期待通り動作するような辞書を定義してください
+print(f"-----A-9:-----")
+kazuma_info = {"first_name": "Kazuma", "family_name": "Takahashi", "age": 35}
 
-# def kazuma_info(fist_name, family_name, age):
-#     kazuma_info = ["Kazuma", "Takahashi", 35]
-#
-#
-# print(kazuma_info["first_name"])  # "Kazuma"
-# print(kazuma_info["family_name""]) # "
-# Takahashi
-# "
-# print(kazuma_info["age"])  # 35
+print(kazuma_info["first_name"])  # "Kazuma"
+print(kazuma_info["family_name"])  # "Takahashi"
+print(kazuma_info["age"])  # 35
+
+print()
+
+# A-10: サイコロ
+# 1から6の整数をランダムに出力する dice() 関数を実装してください
+print(f"-----A-10:-----")
+
+
+def dice():
+    print(random.randint(1, 6))
+
+
+dice()
+
+# A-11: BMIアプリ
+# 身長(m)と体重(kg)を入力として受け取りBMIを計算するアプリを実装してください
+# 参考となるWebアプリ https://keisan.casio.jp/exec/system/1161228732​
+# 小数点第2位まで表示すること
